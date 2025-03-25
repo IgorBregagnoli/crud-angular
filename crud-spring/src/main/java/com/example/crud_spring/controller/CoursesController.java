2b928d6c719b285d0b46cf76058019ab8ce58901
IgorBregagnoli <38503425+IgorBregagnoli@users.noreply.github.com>
2025-03-25 09:57:26 -0300

    swagger

--- crud-spring/src/main/java/com/example/crud_spring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/example/crud_spring/controller/CoursesController.java
@@ -16,6 +16,9 @@ import com.example.crud_spring.dto.CourseDTO;
 import com.example.crud_spring.dto.CoursePageDTO;
 import com.example.crud_spring.service.CourseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
@@ -35,34 +38,51 @@ public class CoursesController {
     this.courseService = courseService;
   }
 
+  @Operation(description = "Busca todos os cursos")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Retorna os cursos")
+  })
   @GetMapping
   public CoursePageDTO list(@RequestParam(defaultValue = "0") @PositiveOrZero int page, @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize){
     return courseService.list(page, pageSize);
   }
 
-  /*
-  @GetMapping
-  public List<CourseDTO> list() {
-    return courseService.list();
-  }
-  */
-
+  @Operation(description = "Busca o curso pelo id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Retorna o curso"),
+    @ApiResponse(responseCode = "400", description = "Não existe o curso informado")
+  })
   @GetMapping("/{id}")
   public CourseDTO findById(@PathVariable @NotNull @Positive Long id){
     return courseService.findById(id);
   }
 
+  @Operation(description = "Cria um novo curso")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Cria um novo curso"),
+    @ApiResponse(responseCode = "400", description = "Não foi possível criar um novo curso informado")
+  })
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO course){
     return courseService.create(course);
   }
 
+  @Operation(description = "Atualiza um curso pelo id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Atualiza o curso"),
+    @ApiResponse(responseCode = "400", description = "Não existe o curso informado")
+  })
   @PutMapping("/{id}")
   public CourseDTO update(@PathVariable @NotNull @Positive Long id, @RequestBody CourseDTO course){
     return courseService.update(id, course);
   }
 
+  @Operation(description = "Deleta um curso pelo id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Deleta o curso"),
+    @ApiResponse(responseCode = "400", description = "Não existe o curso informado")
+  })
   @DeleteMapping("/{id}")
   @ResponseStatus(code = HttpStatus.NO_CONTENT)
   public void delete(@PathVariable @NotNull @Positive Long id){
